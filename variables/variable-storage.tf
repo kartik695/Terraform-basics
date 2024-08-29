3a4fe2e98416ef3ec5a85a66c03f29d29ce6ba4c
kartik695 <arorakartik669@gmail.com>
2024-08-29 15:16:19 +0530

    variables-2

---
+++ variables/variable-storage.tf
@@ -0,0 +1,10 @@
+variable username {
+    type = string
+    default = "World"
+}
+
+variable age {
+    type = number
+    default = 46
+}
+
---
+++ variables/variable.tf
@@ -0,0 +1,21 @@
+//output "hello1" {
+//  value = "Hello World 123"
+//}
+
+
+
+output printname  {
+  value = " Hello my name is ${var.username} and my age is ${var.age}"
+}
+
+
+
+
+
+
+
+
+
+
+
+
